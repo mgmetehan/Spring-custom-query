1d7151a8a060d2ea4bf0b7d28f78b6840c2cb4b5
mgmetehan <mgmetehan@hotmail.com>
2024-03-31 21:26:08 +0300

    init project

---
+++ src/main/java/com/mgmetehan/customquery/CustomQueryApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.customquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomQueryApplication.class, args);
+	}
+
+}
